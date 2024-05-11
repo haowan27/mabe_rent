c605e49dd3d0c80bcca7ff9829fb924784fe37b0
haowan27 <luyiguo86@gmail.com>
2024-05-11 20:22:48 +0800

    add template engine useage eamxple

---
+++ example/wtf.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__, template_folder="../templates")
+app.config["SECRET_KEY"] = "your_secret_key"
+
+
+# 创建登录表单
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
+
+
+# 定义路由
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # 在这里处理登录逻辑，这里只是简单的重定向到首页
+        return redirect(url_for("index"))
+    return render_template("static/login.html", form=form)
+
+
+@app.route("/")
+def index():
+    return str(app.template_folder)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from flask import Flask
+from controller import admin
 import yaml
 
 app = Flask(__name__)
+app.register_blueprint(admin.bp)
+
 
 def load_config():
-    with open('config.yml', 'r') as file:
+    with open("config.yml", "r") as file:
         config = yaml.safe_load(file)
     app.config.update(config)
 
-if __name__ == '__main__':
+
+def load_debug_config():
+    with open("debug-config.yml", "r") as file:
+        config = yaml.safe_load(file)
+    app.config.update(config)
+
+
+if __name__ == "__main__":
     load_config()
     app.run()
+else:
+    load_config()
